e8923946526673690070636c5a72c912ed0adb64
Nathan Eliason <nathaneliason@gmail.com>
2017-10-10 14:31:03 -0400

    Finished initial working version.

---
+++ exercise1.js
@@ -0,0 +1,88 @@
+// Simulates the weighing of a single m&m
+function weighMM(expectedWeight, scalePrecision) {
+    let weight = expectedWeight;
+    let mutationChance = 0.6;
+    let direction = (Math.random() > 0.5);
+    while (Math.random() < mutationChance) {
+        weight += scalePrecision * (direction ? 1 : -1);
+        mutationChance *= mutationChance;
+    }
+    return weight;
+}
+
+function mmax(i, j) { return Math.max(i,j); }
+function mmin(i, j) { return Math.min(i,j); }
+
+// Given an array of weights, bins them into numBins
+// bins and returns the object:
+// {
+//    binSize: float,
+//    minWeight: float,
+//    bins: [int, int, ...]
+// }
+function binWeights(weights, numBins, middle) {
+    let maxWeight = weights.reduce(mmax);
+    let minWeight = weights.reduce(mmin);
+    let range = 2 * mmax(maxWeight - middle, middle - minWeight);
+    minWeight = mmin(minWeight, middle - range/2);
+    let binSize = range / numBins;
+    let bins = Array.apply(null, Array(numBins)).map(i => 0);
+    weights.forEach(w => {
+        bins[mmin(bins.length-1, Math.floor((w-minWeight)/binSize))]++;
+    });
+    return {binSize, minWeight, bins};
+}
+
+// Plots a distribution in the HTML object #mm-dist.
+function plotDistribution(binSize, minWeight, bins, label, $dist, title) {
+    let $bars = $dist.children('.bars').html('');
+    let binWidth = $bars.width() / bins.length;
+    let maxValue = bins.reduce(mmax);
+    $dist.children('.header').html(title);
+    bins.forEach((n, i) => {
+        $('<div></div>')
+            .css({
+                height: '0%',
+                width: binWidth + 'px',
+                left: (i*binWidth) + 'px'
+            })
+            .attr('data-height', (100*n/maxValue) + '%')
+            .append('<div class="x-label">' + 
+                (minWeight + i*binSize).toFixed(2) + label + 
+                ' - ' + 
+                (minWeight + (i+1)*binSize).toFixed(2) + label + '</div>')
+            .append('<div class="y-label"><div>' + n + '<div></div>')
+            .appendTo($bars);
+    });
+    setTimeout(function() {
+        // Set actual height of each bar appropriately once rendered.
+        $bars.children().each((i, bar) => {
+            console.log(bar);
+            $(bar).css('height', $(bar).attr('data-height'))
+        });
+    }, 50);
+
+}
+
+function MMDist() {
+    createMMDistribution(/*numMMs=*/Number($("input[name=mm-num]").val()), 
+                         /*eMM=*/Number($("input[name=mm-weight]").val()), 
+                         /*scalePrecision=*/Number($("input[name=mm-precision]").val()), 
+                         /*numBins=*/Number($("input[name=mm-bins]").val()),
+                         /*label=*/'g');
+}
+
+function createMMDistribution(numMMs, eMM, scalePrecision, numBins, label) {
+    $("#mm-dist").toggleClass('compact', numBins > 8);
+    let MMs = Array.apply(null, Array(numMMs)).map(i => weighMM(eMM, scalePrecision))
+    console.log(MMs);
+    let binObj = binWeights(MMs, numBins, eMM);
+    console.log(binObj);
+    plotDistribution(binObj.binSize, binObj.minWeight, binObj.bins, label, 
+        $("#mm-dist"), 'Measuring the mass of ' + numMMs + ' M&amp;M\'s (' + label + ')');
+}
+
+$(function() {
+    MMDist();
+    $("#exercise-1-variables input").change(MMDist);
+});
---
+++ exercise2.js
@@ -0,0 +1,49 @@
+
+
+// Returns true iff the site released.
+function simulateReleaseSite(successProb) {
+    return (Math.random() < successProb);
+}
+
+// Returns the number of successful sites.
+function simulatePresynapticTerminal(successProb, numSites) {
+    return Array.apply(null, Array(numSites))
+        .map(i => simulateReleaseSite(successProb))
+        .reduce((i,j) => i + j);
+}
+
+// Returns the amplitude (in M&M grams) of a single EPSP of a 
+// single site.
+function simulateEPSP(successProb, numSites, eMM, scalePrecision) {
+    return Array.apply(null, Array(numSites))
+    .map(i => weighMM(eMM, scalePrecision) * simulateReleaseSite(successProb))
+    .reduce((i,j) => i + j);
+}
+
+// Simulates a presynaptic terminal with `numSites` vesicle
+// release sites, each with probability of releasing `successProb`,
+// `numTrials` times, and generates a distribution.
+function generateHistogram(successProb, numSites, numTrials, eMM, scalePrecision, numBins) {
+    $("#mv-dist").toggleClass('compact', numBins > 8);
+    let weights = Array.apply(null, Array(numTrials))
+        .map(i => simulateEPSP(successProb, numSites, eMM, scalePrecision));
+    let middle = (weights.reduce(mmax) + weights.reduce(mmin)) / 2;
+    let binObj = binWeights(weights, numBins, middle);
+
+    plotDistribution(binObj.binSize, binObj.minWeight, binObj.bins, 'mV',
+        $("#mv-dist"), 'Measuring the amplitude of M&amp;M EPSPs (mV)');
+}
+
+$(function() {
+    mvDist();
+    $("input").change(mvDist);
+})
+
+function mvDist() {
+    generateHistogram(/*successProb=*/Number($("input[name=mv-prob]").val()),
+                      /*numSites=*/Number($("input[name=mv-sites]").val()),
+                      /*numTrials=*/Number($("input[name=mv-trials]").val()),
+                      /*eMM=*/Number($("input[name=mm-weight]").val()),
+                      /*scalePrecision=*/Number($("input[name=mm-precision]").val()),
+                      /*numBins=*/Number($("input[name=mv-bins]").val()))
+}
